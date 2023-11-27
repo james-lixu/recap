3fde6b84504bfec8473467a508ae254bb88f6b69
James Li-Xu <36280940+james-lixu@users.noreply.github.com>
2023-11-27 03:32:40 -0500

    image uploader implemented
    image appears in its correct div on screen when uploaded

--- src/_root/forms/PostForm.tsx
+++ src/_root/forms/PostForm.tsx
@@ -1,12 +1,11 @@
+import React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { format } from "date-fns";
-import { useForm } from "react-hook-form";
 import * as z from "zod";
+import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -14,16 +13,16 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import FileUploader from "@/components/shared/FileUploader";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
 import { Calendar } from "@/components/ui/calendar";
-import FileUploader from "@/components/shared/FileUploader";
 import { Calendar as CalendarIcon } from "lucide-react";
-import { cn } from "@/lib/utils"
-import React from "react";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -31,7 +30,7 @@ const formSchema = z.object({
   }),
 });
 
-const PostForm = () => {
+const PostForm = ({ post }) => {
   const [date, setDate] = React.useState<Date>();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -61,7 +60,10 @@ const PostForm = () => {
             <FormItem>
               <FormLabel className="shad-form_label ">Add Photos</FormLabel>
               <FormControl>
-                <FileUploader />
+                <FileUploader
+                  fieldChange={field.onChange}
+                  mediaUrl={post?.imageUrl}
+                />
               </FormControl>
               <FormMessage className="shadcn-form_message" />
             </FormItem>
@@ -84,28 +86,35 @@ const PostForm = () => {
           )}
         />
 
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              variant={"outline"}
-              className={cn(
-                "w-[280px] justify-start text-left font-normal",
-                !date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              {date ? format(date, "PPP") : <span>Pick a date</span>}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0">
-            <Calendar
-              mode="single"
-              selected={date}
-              onSelect={setDate}
-              initialFocus
-            />
-          </PopoverContent>
-        </Popover>
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label">Add Location</FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-inputPost" {...field} />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label ">
+                Add Tags (separated by comma " , ")
+              </FormLabel>
+              <FormControl>
+                <Input type="text" className="shad-inputPost" {...field} />
+              </FormControl>
+              <FormMessage className="shad-form_message" />
+            </FormItem>
+          )}
+        />
 
         <Button
           type="submit"
--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,27 +1,61 @@
-import React, { useCallback, useState } from "react";
-import { useDropzone } from "react-dropzone";
+import { useCallback, useState } from "react";
+import { FileWithPath, useDropzone } from "react-dropzone";
+import { Button } from "../ui/button";
 
-const FileUploader = () => {
-  const [fileUrl, setfileUrl] = useState('');
+type FileUploaderProps = {
+  fieldChange: (FILES: File[]) => void;
+  mediaUrl: string;
+};
 
-  const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
+  const [file, setFile] = useState<File[]>([]);
+  // const [fileUrl, setFileUrl] = useState<string>(mediaUrl);
+  const [fileUrl, setFileUrl] = useState("");
+
+  const onDrop = useCallback(
+    (acceptedFiles: FileWithPath[]) => {
+      setFile(acceptedFiles);
+      fieldChange(acceptedFiles);
+      setFileUrl(URL.createObjectURL(acceptedFiles[0]));
+    },
+    [file]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: {
+      "image/*": [".png", ".jpeg", ".jpg", ".svg"],
+    },
+  });
 
   return (
-    <div {...getRootProps()} className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer">
-      <input {...getInputProps()} className="cursor-pointer"/>
-      { fileUrl ? (
-                <div>
-                test 1
-              </div>
-      ) : (
-<div className="file_uploader-box focus:ring-2 focus:ring-offset-2 focus:ring-clay-ash">
+    <div
+      {...getRootProps()}
+      className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer"
+    >
+      <input {...getInputProps()} className="cursor-pointer" />
+      {fileUrl ? (
+        <div className="flex flex-1 justify-center w-full padding-5 lg:p-10">
           <img 
+            src={fileUrl}
+            alt="uploaded-image"
+            className="file_uploader-img"
+          />
+        </div>
+      ) : (
+        <div className="file_uploader-box focus:ring-2 focus:ring-offset-2 focus:ring-clay-ash">
+          <img
             src="/assets/icons/file-upload.svg"
             alt="file-upload"
+            width={128}
           />
+
+          <h3 className="base-medium text-raffia mb-6 mt-6 font-handlee">
+            Drop photo here
+          </h3>
+          <Button className="shad-button_dark_4 bg-willow-grove mt-4">
+            Upload a photo
+          </Button>
         </div>
       )}
     </div>
