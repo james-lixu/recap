cbae44fb93eefcb0877724c03ed011c161cbc21c
James Li-Xu <36280940+james-lixu@users.noreply.github.com>
2023-11-26 19:55:48 -0500

    front end implemented for create post page

--- src/_root/forms/PostForm.tsx
+++ src/_root/forms/PostForm.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { format } from "date-fns";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { Button } from "@/components/ui/button";
@@ -13,7 +14,16 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
 import FileUploader from "@/components/shared/FileUploader";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils"
+import React from "react";
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -22,10 +32,12 @@ const formSchema = z.object({
 });
 
 const PostForm = () => {
+  const [date, setDate] = React.useState<Date>();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      date: null,
     },
   });
 
@@ -44,6 +56,19 @@ const PostForm = () => {
       >
         <FormField
           control={form.control}
+          name="file"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="shad-form_label ">Add Photos</FormLabel>
+              <FormControl>
+                <FileUploader />
+              </FormControl>
+              <FormMessage className="shadcn-form_message" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
           name="caption"
           render={({ field }) => (
             <FormItem>
@@ -58,20 +83,36 @@ const PostForm = () => {
             </FormItem>
           )}
         />
-        <FormField
-          control={form.control}
-          name="file"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel className="shad-form_label">Add Photos</FormLabel>
-              <FormControl>
-                <FileUploader />
-              </FormControl>
-              <FormMessage className="shadcn-form_message" />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
+
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={"outline"}
+              className={cn(
+                "w-[280px] justify-start text-left font-normal",
+                !date && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date ? format(date, "PPP") : <span>Pick a date</span>}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode="single"
+              selected={date}
+              onSelect={setDate}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+
+        <Button
+          type="submit"
+          className="shad-button_primary text-silver-sand font-handlee font-semibold hover:text-raffia"
+        >
+          Upload
+        </Button>
       </form>
     </Form>
   );
--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,19 +1,28 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 const FileUploader = () => {
+  const [fileUrl, setfileUrl] = useState('');
+
   const onDrop = useCallback((acceptedFiles) => {
     // Do something with the files
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
+    <div {...getRootProps()} className="flex flex-center flex-col bg-dark-3 rounded-xl cursor-pointer">
+      <input {...getInputProps()} className="cursor-pointer"/>
+      { fileUrl ? (
+                <div>
+                test 1
+              </div>
       ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
+<div className="file_uploader-box focus:ring-2 focus:ring-offset-2 focus:ring-clay-ash">
+          <img 
+            src="/assets/icons/file-upload.svg"
+            alt="file-upload"
+          />
+        </div>
       )}
     </div>
   );
